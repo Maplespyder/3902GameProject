0103e0c8f309aad21c10342082b0bbb9a4fad93e
Sam Trabitz <trabitz.4@osu.edu>
2017-09-17 08:30:27 -0400

    Added IState

---
+++ MarioClone/States/IState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioClone.States
+{
+    public interface IState<TContext>
+    {
+        TContext Context { get; }
+
+        void CheckNextState();
+    }
+}
