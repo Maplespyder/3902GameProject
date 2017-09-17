ace53df92ac7874d982a2eda4e3c34179c36773d
Sam Trabitz <trabitz.4@osu.edu>
2017-09-17 08:46:47 -0400

    Added abstract state and an example of a Mario state

---
+++ MarioClone/States/MarioActionState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioClone.States
+{
+    abstract class MarioActionState : State<MarioObject>
+    {
+        public MarioActionState(MarioObject context) : base(context)
+        {
+        }
+
+        // mario based methods go here
+        public abstract void Jump();
+    }
+
+    internal class MarioObject
+    {
+    }
+}
---
+++ MarioClone/States/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioClone.States
+{
+    public abstract class State<TContext>
+        where TContext : class
+    {
+        protected TContext Context { get; set; }
+
+        public State(TContext context)
+        {
+            Context = context;
+        }
+
+        protected abstract void CheckNextState();
+    }
+}
