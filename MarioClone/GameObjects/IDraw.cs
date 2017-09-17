d601614ed9db66d9ccf1f8abccc6bcdb0c03a786
Sam Trabitz <trabitz.4@osu.edu>
2017-09-17 12:26:44 -0400

    Syntax and structure fixes

--- MarioClone/GameObjects/IDraw.cs
+++ MarioClone/GameObjects/IDraw.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace MarioClone.GameObjects
 {
-    public interface IMoveable
+    public interface IDraw
     {
-        void Move();
-    
-        Vector2 Velocity { get; set; }
+        int DrawOrder { get; }
+        bool Visible { get; }
 
+        void Draw(SpriteBatch spriteBatch);
     }
 }
