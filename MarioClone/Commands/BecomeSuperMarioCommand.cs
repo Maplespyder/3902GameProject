b0441c122f769f8019faefaeca8fa654eeb2510e
Jacob Crabtree <crabtree.234@buckeyemail.osu.edu>
2017-09-20 13:33:24 -0400

    Added a few commands for super mario and hidden blocks, they aren't done yet because the actual game objects don't exist yet.

---
+++ MarioClone/Commands/BecomeSuperMarioCommand.cs
@@ -0,0 +1,20 @@
+﻿/*using MarioClone.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioClone.Commands
+{
+    public class BecomeSuperMarioCommand : AbstractCommand<Mario>
+    {
+        public BecomeSuperMarioCommand(Mario m) : base(m) { }
+
+        public override void InvokeCommand()
+        {
+            Receiver.BecomeSuperMario();
+        }
+    }
+}
+*/
