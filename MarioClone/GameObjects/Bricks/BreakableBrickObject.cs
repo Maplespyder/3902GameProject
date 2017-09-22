2e5ea93b5c1b4d86e740e5a77345716b3d325233
Allie Jett <jett.30@buckeyemail.osu.edu>
2017-09-22 10:07:05 -0400

    WIP of adding methods and functionality to BrickObject and formed BrickPieces.

--- MarioClone/GameObjects/Bricks/BreakableBrickObject.cs
+++ MarioClone/GameObjects/Bricks/BreakableBrickObject.cs
@@ -1,44 +1,78 @@
 ﻿using System;
 using MarioClone.Sprites;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using MarioClone.GameObjects.Bricks;
+using MarioClone.Factories;
 
 /// <summary>
 /// Summary description for Class1
 /// </summary>
 namespace MarioClone.GameObjects
 {
-    public class BreakableBrickObject : IGameObject
+    public class BreakableBrickObject : IGameObject, IMoveable, IDraw
 	{
         public Vector2 Position { get; protected set; }
 
         public Vector2 Velocity { get; }
 
-        int DrawOrder { get; }
+        public int DrawOrder { get; }
 
-        bool Visible { get; }
+        public bool Visible { get; protected set; }
 
         public ISprite Sprite { get; protected set; }
 
-        public BreakableBrickObject(ISprite sprite, Vector2 velocity, Vector2 position)
+		private List<IGameObject> PieceList = new List<IGameObject>();
+
+		public BreakableBrickObject(ISprite sprite, Vector2 velocity, Vector2 position)
         {
             Sprite = sprite;
             Velocity = velocity;
             Position = position;
-
+			Visible = true;
         }
 
-        public void BrickBump()
+        public void HitByMario()
         {
-
+			//if small
+			Bump();
+			//else big
+			Break();
         }
 
-        public void BrickBreak()
+        public void Break()
         {
+			//TODO: Have the Brick cease drawing & create 4 nuggets
+			//Create nuggets 
+			for(int i = 0; i < 4; i++)
+			{
+				var piece = MarioFactory.Create(BlockType BrokenBlock, Position); 
+				PieceList.Add(piece);
+			}
+
+        }
+		public void Bump()
+		{
+			//TODO: Move it up and down
+		}
+
+		public void Update(GameTime gameTime)
+        {
+           
         }
 
-        public void Update(GameTime gameTime)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public void Move()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Draw(SpriteBatch spriteBatch, float layer, GameTime gameTime)
+		{
+			if (Visible)
+			{
+				Sprite.Draw(spriteBatch, Position, layer, gameTime);
+			}
+		}
+	}
 }
---
+++ MarioClone/GameObjects/Bricks/BrickPieceObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using MarioClone.Sprites;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MarioClone.GameObjects.Bricks
+{
+	public class BrickPieceObject : IGameObject, IDraw, IMoveable
+	{
+		public Vector2 Position { get; protected set; }
+
+		public Vector2 Velocity { get; }
+
+		public int DrawOrder { get; }
+
+		public bool Visible { get; protected set; }
+
+		public ISprite Sprite { get; protected set; }
+
+		public BrickPieceObject(ISprite sprite, Vector2 velocity, Vector2 position)
+		{
+			Sprite = sprite;
+			Velocity = velocity;
+			Position = position;
+			Visible = true;
+
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			//TODO UPDATE NUGGET MOVEMENT
+		}
+
+		public void Move()
+		{
+			
+		}
+
+		public void Draw(SpriteBatch spriteBatch, float layer, GameTime gameTime)
+		{
+			if (Visible)
+			{
+				Sprite.Draw(spriteBatch, Position, layer, gameTime);
+			}
+		}
+	}
+}
+}
