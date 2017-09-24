50003d38ac2815e700aa135235b38c14c22aab0a
Allie Jett <jett.30@buckeyemail.osu.edu>
2017-09-24 12:37:13 -0400

    Paused implemented

--- MarioClone/MarioCloneGame.cs
+++ MarioClone/MarioCloneGame.cs
@@ -19,7 +19,7 @@ namespace MarioClone
 		static GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
         KeyboardController keyboardController;
-        GamepadController gamepadController;
+		GamepadController gamepadController;
         
         static ContentManager _content;
         List<IGameObject> gameObjects;
@@ -57,7 +57,7 @@ namespace MarioClone
 
 			gameObjects = new List<IGameObject>();
 			keyboardController.AddInputCommand((int)Keys.Q, new ExitCommand(this));
-			gamepadController.AddInputCommand((int)Buttons.Start, new ExitCommand(this));
+			gamepadController.AddInputCommand((int)Buttons.BigButton, new ExitCommand(this));
 
 			var gameBounds = new List<Rectangle>()
 			{
@@ -124,20 +124,23 @@ namespace MarioClone
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
 
-            keyboardController.UpdateAndExecuteInputs();
-            gamepadController.UpdateAndExecuteInputs();
+			if (!paused)
+			{
+				keyboardController.UpdateAndExecuteInputs();
+				gamepadController.UpdateAndExecuteInputs();
 
-            List<IGameObject> tempList = new List<IGameObject>();
-            foreach(var obj in gameObjects)
-            {
-                if(obj.Update(gameTime))
-                {
-                    tempList.Add(obj);
-                }
-            }
+				List<IGameObject> tempList = new List<IGameObject>();
+				foreach (var obj in gameObjects)
+				{
+					if (obj.Update(gameTime))
+					{
+						tempList.Add(obj);
+					}
+				}
 
-            gameObjects.RemoveAll((x) => tempList.Remove(x));
-			base.Update(gameTime);
+				gameObjects.RemoveAll((x) => tempList.Remove(x));
+				base.Update(gameTime);
+			}
 		}
 
 		/// <summary>
@@ -166,6 +169,13 @@ namespace MarioClone
 		public static GraphicsDeviceManager ReturnGraphicsDevice
 		{
 			get { return graphics; }
+		}
+
+		private static bool paused = false;
+		public static bool Paused
+		{
+			set { paused = value; }
+			get { return paused; }
 		}
 
 		public void ExitCommand()
