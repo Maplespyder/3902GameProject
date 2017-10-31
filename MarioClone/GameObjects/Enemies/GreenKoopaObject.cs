5f4bbe3af72635d4b82abfebceb80167b494c012
Anna Wolfe <wolfe.765@buckeyemail.osu.edu>
2017-10-31 15:49:25 -0400

    added movement in for koopas, made some changes to my bitmap

--- MarioClone/GameObjects/Enemies/GreenKoopaObject.cs
+++ MarioClone/GameObjects/Enemies/GreenKoopaObject.cs
@@ -13,27 +13,41 @@ namespace MarioClone.GameObjects
 			BoundingBox.UpdateOffSets(-8, -8, -8, -8);
             BoundingBox.UpdateHitBox(Position, Sprite);
             PowerupState = new KoopaAlive(this);
+            Velocity = new Vector2(-EnemyHorizontalMovementSpeed, Velocity.Y);
         }
 
         public override bool CollisionResponse(AbstractGameObject gameObject, Side side, GameTime gameTime)
         {
             if (gameObject is Mario)
             {
-                if(side.Equals(Side.Top))
+                if (side.Equals(Side.Top))
                 {
                     PowerupState.BecomeDead();
                     TimeDead = 0;
                     return true;
 
                 }
-                
+
             }
-            return false;
+            else if (gameObject is AbstractBlock)
+            {
+                if (side == Side.Left)
+                {
+                    Velocity = new Vector2(EnemyHorizontalMovementSpeed, Velocity.Y);
+                }
+                else if (side == Side.Right)
+                {
+                    Velocity = new Vector2(-EnemyHorizontalMovementSpeed, Velocity.Y);
+                }
+            }
+                return false;
+
 
         }
 
         public override bool Update(GameTime gameTime, float percent)
         {
+            Position = new Vector2(Position.X + Velocity.X, Position.Y + Velocity.Y);
             bool retVal = PowerupState.Update(gameTime, percent);
             return base.Update(gameTime, percent) || retVal;
         }
