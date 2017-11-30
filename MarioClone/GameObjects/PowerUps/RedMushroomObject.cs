2ac5759b0952a3acab5fb9d112be830f6390fd49
Allie Jett <jett.30@buckeyemail.osu.edu>
2017-11-30 17:07:01 -0500

    Fixed berry object hitbox

--- MarioClone/GameObjects/PowerUps/RedMushroomObject.cs
+++ MarioClone/GameObjects/PowerUps/RedMushroomObject.cs
@@ -18,7 +18,7 @@ namespace MarioClone.GameObjects
         public RedMushroomObject(ISprite sprite, Vector2 position) : base(sprite, position, Color.Green)
         {
             DrawOrder = .51f;
-            BoundingBox.UpdateOffSets(-1, -1, 0, 0);
+            BoundingBox.UpdateOffSets(-1, -1, 0, -2);
             PointValue = 1000;
         }
 
